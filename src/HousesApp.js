bd4eb8680bbca3f05c6359751be44d7dc5b33f3c
Russ Rinzler <russ.rinzler@gmail.com>
2019-07-20 16:37:04 +0200

    Initial commit: working webpack with react, es6 and css

---
+++ src/HousesApp.js
@@ -0,0 +1,11 @@
+import React from 'react';
+import ReactDOM from 'react-dom';
+
+import './main.css';
+
+const HousesApp = () => {
+	return <p>it a house app</p>;
+};
+
+export default HousesApp;
+ReactDOM.render(<HousesApp />, document.getElementById("houses-app"));
