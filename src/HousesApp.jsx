b882006617c10e7025d7934c876660646a7c681f
Russ Rinzler <russ.rinzler@gmail.com>
2019-07-20 23:40:41 +0200

    fetch houses using the api provided

--- src/HousesApp.jsx
+++ src/HousesApp.jsx
@@ -1,7 +1,28 @@
-import React from 'react';
-
+import React, { useEffect, useState } from 'react';
 import './main.css';
 
-const HousesApp = () => <p>it a house app</p>;
+const HousesApp = () => {
+	const [hasError, setError] = useState(false);
+	const [houses, setHouses] = useState({});
+
+	const fetchHouses = () => {
+		fetch('http://localhost:1337/houses')
+			.then(response => response.json())
+			.then(json => setHouses(json))
+			.catch(() => setError(true));
+	};
+
+	useEffect(() => {
+		fetchHouses();
+	}, []);
+
+	return (
+		<>
+			{hasError && <span>here is an error</span>}
+			<span>houses are here!</span>
+			<p>{JSON.stringify(houses)}</p>
+		</>
+	);
+};
 
 export default HousesApp;
