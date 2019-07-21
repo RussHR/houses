0e3dea683eb1e7fdd8114fb55a4e37688991e8a3
Russ Rinzler <russ.rinzler@gmail.com>
2019-07-21 18:10:06 +0200

    add a sorting callback at the app level and unfortunately remove a11y linting rules that were causing issues

--- src/HousesApp.jsx
+++ src/HousesApp.jsx
@@ -7,28 +7,43 @@ import './main.css';
 const HousesApp = () => {
 	const [hasError, setError] = useState(false);
 	const [vendors, setVendors] = useState({});
+	const [sortMode, setSortMode] = useState('');
 
+	/** house fetching functionality */
 	const fetchHouses = () => {
 		fetch('http://localhost:1337/houses')
 			.then(response => response.json())
 			.then(json => setVendors(formatJsonForState(json)))
 			.catch(() => setError(true));
 	};
-
 	useEffect(() => {
 		fetchHouses();
 	}, []);
+
+	/** sort mode setting functionality */
+	const onChangeSortMode = ({ currentTarget: { value } }) => {
+		setSortMode(value);
+	};
 
 	return (
 		<>
 			{hasError && <span>here is an error</span>}
 			<span>houses are here!</span>
+			<label htmlFor="sort-mode">Sort by:</label>
+			<select id="sort-mode" onChange={onChangeSortMode}>
+				<option value="">--</option>
+				<option value="name">Name</option>
+				<option value="price">Price</option>
+				<option value="size">Size</option>
+				<option value="id">ID</option>
+			</select>
 			{Object.entries(vendors).map(([vendorId, { displayName, logoThumb, houses }]) => (
 				<Vendor
 					key={vendorId}
 					displayName={displayName}
 					logoThumb={logoThumb}
 					houses={houses}
+					sortMode={sortMode}
 				/>
 			))}
 		</>
